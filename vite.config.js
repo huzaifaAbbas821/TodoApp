becbd20440760e74e4958a07885e41ac1a0ae7f0
huzaifaAbbas821 <huzaifaabbas8121@gmail.com>
2024-07-14 11:51:20 +0500

    changes again again

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-<<<<<<< HEAD
-
-=======
-    base: "/TodoApp/"
->>>>>>> 8b23ffb907c17557eb8b46866c1261e4636fddee
+  plugins: [react()]
 })
